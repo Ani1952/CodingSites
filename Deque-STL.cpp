47f8c97103e7507db8ba2e8cdf114028966b5b25
Ani1952 <heroda02@gmail.com>
2021-10-09 17:08:08 +0530

    added

---
+++ Deque-STL.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int T;
+    cin >> T;
+    while (T)
+    {
+        int size, subsize,n;
+        cin >> size >> subsize;
+        cout<<endl;
+        int *a=new int(size);
+        for (int i = 0; i < size; i++)
+        {
+            cin>>n;
+            a[i]=n;
+        }
+
+        deque<int> d;
+        for(int i=0;i<size;i++){
+            
+        }
+        
+    }
+
+    return 0;
+}
