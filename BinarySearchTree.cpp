b5a50add20ce090e3ffeb8ca7609acef11049af0
Ani1952 <heroda02@gmail.com>
2021-11-30 10:57:54 +0530

    Update BinarySearchTree.cpp

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -16,6 +16,13 @@ public:
     }
 };
 
+class LinkedList
+{
+public:
+    Node *head;
+    Node *tail;
+};
+
 Node *insert(Node *root, int key)
 {
     if (root == NULL)
@@ -27,6 +34,75 @@ Node *insert(Node *root, int key)
         root->right = insert(root->right, key);
 
     return root;
+}
+
+LinkedList tree2LL(Node *root)
+{
+    LinkedList l;
+    if (root == NULL)
+    {
+        l.head = l.tail = NULL;
+        return l;
+    }
+
+    if (root->left == NULL and root->right == NULL)
+    {
+        l.head = l.tail = root;
+    }
+
+    else if (root->left != NULL and root->right == NULL)
+    {
+        LinkedList leftll = tree2LL(root->left);
+        leftll.tail->right = root;
+        l.head = leftll.head;
+        l.tail = root;
+    }
+    else if (root->left == NULL and root->right != NULL)
+    {
+        LinkedList rightll = tree2LL(root->right);
+        root->right = rightll.head;
+        l.head = root;
+        l.tail = rightll.tail;
+    }
+
+    else
+    {
+        LinkedList leftll = tree2LL(root->left);
+        LinkedList rightll = tree2LL(root->right);
+
+        leftll.tail->right = root;
+        root->right = rightll.head;
+
+        l.head = leftll.head;
+        l.tail = rightll.tail;
+    }
+
+    return l;
+}
+int nearest(Node *root, int ele)
+{
+
+    int close;
+    int diff = INT_MAX;
+    Node *temp = root;
+
+    while (temp != NULL)
+    {
+        int curr = abs(temp->data - ele);
+        if (curr == 0)
+            return temp->data;
+
+        if (curr < diff)
+        {
+            diff = curr;
+            close = temp->data;
+        }
+
+        if (temp->data < ele)
+            temp = temp->right;
+        temp = temp->left;
+    }
+    return close;
 }
 
 bool search(Node *root, int key)
@@ -57,10 +133,10 @@ int main()
 {
     Node *root = NULL;
     cout << "Enter size of TREE" << endl;
-    int n, a,ele;
+    int n, a, ele;
     cin >> n;
     vector<int> arr;
-    cout<<"Enter Elements\n";
+    cout << "Enter Elements\n";
     for (int i = 0; i < n; i++)
     {
         cin >> a;
@@ -72,8 +148,8 @@ int main()
     InOrder(root);
     cout << endl;
 
-    cout<<"Enter Element to search"<<endl;
-    cin>>ele;
+    cout << "Enter Element to search" << endl;
+    cin >> ele;
     (search(root, ele))
         ? cout << "Found" << endl
         : cout << "Not Found" << endl;
